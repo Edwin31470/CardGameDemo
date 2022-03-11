56365ddd16f2d4789b5121b5d8bb09fba8d92b4c
Edwin31470 <edwinjacobs@hotmail.co.uk>
2022-03-11 15:21:36 +0000

    Add Delegates to assembly

---
+++ Assets/Scripts/Delegates.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.Cards;
+using Assets.Scripts.Events;
+
+namespace Assets.Scripts
+{
+    // Shorthand for Func<IEnumerable<BaseCard>,IEnumerable<BaseEvent>>
+    public delegate IEnumerable<BaseEvent> FinishSelection(IEnumerable<BaseCard> cards);
+
+    public delegate IEnumerable<BaseEvent> FinishAction(BaseCard owner);
+}
