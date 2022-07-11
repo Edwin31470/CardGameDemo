e98954813f197b9191a1f559e833a7ad0fec5598
Lewis Jacobs <lewis@redbite.com>
2022-07-11 22:13:51 +0100

    Added remaining Green cards

--- Assets/Scripts/Effects/CardEffects/GreenCreatureEffects.cs
+++ Assets/Scripts/Effects/CardEffects/GreenCreatureEffects.cs
@@ -5,7 +5,6 @@ using Assets.Scripts.Extensions;
 using System.Collections.Generic;
 using System.Linq;
 
-
 namespace Assets.Scripts.Effects.CardEffects
 {
 
@@ -21,4 +20,91 @@ namespace Assets.Scripts.Effects.CardEffects
         }
     }
 
+    public class ChromaticBasilisk : BaseTargetingEffect<CreatureCard>
+    {
+        public override int Id => 13;
+
+        protected override TargetConditions GetTargetConditions(CreatureCard source, BoardState boardState) => new()
+        {
+            CardType = CardType.Creature,
+            SubType = SubType.Human
+        };
+
+        public override IEnumerable<BaseEvent> GetEffect(CreatureCard source, BoardState board)
+        {
+            yield return new DamageTargetsEvent<CreatureCard>(source, new(), 1, 0);
+        }
+    }
+
+    public class SteadfastBehemoth: CustomInteruptEffect<CreatureCard>
+    {
+        public override int Id => 14;
+
+        public override bool TryInterupt(CreatureCard source, BoardState boardState, BaseEvent triggeringEvent)
+        {
+            if (triggeringEvent is not DamageCreatureEvent<CreatureCard> damageEvent)
+                return false;
+
+            damageEvent.Value = 0;
+
+            return true;
+        }
+    }
+
+    public class TurtleshieldVanguard : BaseSourceEffect<CreatureCard>
+    {
+        public override int Id => 15;
+
+        public override IEnumerable<BaseEvent> GetEffect(CreatureCard source, BoardState board)
+        {
+            var player = board.GetSourceOwner(source).PlayerType;
+
+            yield return new AddTokensEvent(player, TokenType.Shell, 2);
+        }
+    }
+
+    public class BrideOfTheForest : BaseTargetingEffect<CreatureCard>
+    {
+        public override int Id => 16;
+                
+        protected override TargetConditions GetTargetConditions(CreatureCard source, BoardState boardState) => new()
+        {
+            PlayerType = boardState.GetSourceOwner(source).PlayerType.Opposite(),
+            CardType = CardType.Creature,
+            MaxDefence = 8
+        };
+
+
+        public override IEnumerable<BaseEvent> GetEffect(CreatureCard source, BoardState board)
+        {
+            yield return new GainControlEvent<CreatureCard>(source);
+        }
+    }
+
+    public class TailwingMoth : BaseSourceEffect<CreatureCard>
+    {
+        public override int Id => 17;
+
+        public override IEnumerable<BaseEvent> GetEffect(CreatureCard source, BoardState board)
+        {
+            var player = board.GetSourceOwner(source).PlayerType;
+
+            yield return new AddLifePlayerEvent(player, 5);
+        }
+    }
+
+    public class ArishiKingOfLeaves : BaseSourceEffect<CreatureCard>
+    {
+        public override int Id => 18;
+
+        public override IEnumerable<BaseEvent> GetEffect(CreatureCard source, BoardState board)
+        {
+            var player = board.GetSourceOwner(source);
+
+            var greenMana = player.GetManaAmount(Colour.Green);
+
+            yield return new StrengthenCreatureEvent<CreatureCard>(source, source, greenMana * 2);
+            yield return new FortifyCreatureEvent<CreatureCard>(source, source, greenMana * 2);
+        }
+    }
 }
